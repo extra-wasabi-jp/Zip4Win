c7782d2015c211cb8ee72b668e90cd3982c15707
Akizawa, Shuji <extra.wasabi.jp@gmail.com>
2024-11-30 12:39:44 +0900

    各種開発環境のバージョンを最新化

--- src/main/java/com/futsalud/tool/Zip4WinGenerator.java
+++ src/main/java/com/futsalud/tool/Zip4WinGenerator.java
@@ -1,40 +1,50 @@
 package com.futsalud.tool;
 
-import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.ZipFile;
 import net.lingala.zip4j.model.ZipParameters;
-import net.lingala.zip4j.util.Zip4jConstants;
-import org.eclipse.collections.api.factory.Lists;
+import net.lingala.zip4j.model.enums.AesKeyStrength;
+import net.lingala.zip4j.model.enums.CompressionLevel;
+import net.lingala.zip4j.model.enums.CompressionMethod;
+import net.lingala.zip4j.model.enums.EncryptionMethod;
 import org.eclipse.collections.api.list.ImmutableList;
-import org.eclipse.collections.api.list.MutableList;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
 
 public class Zip4WinGenerator {
 
-    public static int generate(final String zipFileName, final char[] password, ImmutableList<File> fileList) {
+    public static int generate(
+            final String zipFileName,
+            final boolean isCheckedForWin,
+            final char[] password,
+            ImmutableList<File> fileList
+    ) {
 
-        try {
-            ZipFile zipFile = new ZipFile(zipFileName);
-            zipFile.setFileNameCharset("Windows-31j");
+        try (final ZipFile zipFile = (password.length != 0)
+                ? new ZipFile(zipFileName, password)
+                : new ZipFile(zipFileName)) {
+            if (isCheckedForWin) {
+                zipFile.setCharset(Charset.forName("MS932"));
+            }
             ZipParameters params = new ZipParameters();
+            params.setFileNameInZip(zipFileName);
             params.setEncryptFiles(false);
-            params.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
-            params.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
-            params.setEncryptionMethod(Zip4jConstants.ENC_METHOD_STANDARD);
-            params.setAesKeyStrength(Zip4jConstants.AES_STRENGTH_256);
+            params.setCompressionMethod(CompressionMethod.DEFLATE);
+            params.setCompressionLevel(CompressionLevel.NORMAL);
+            params.setEncryptionMethod(EncryptionMethod.ZIP_STANDARD);
+            params.setAesKeyStrength(AesKeyStrength.KEY_STRENGTH_256);
             if (password.length != 0) {
                 params.setEncryptFiles(true);
-                params.setPassword(password);
             }
-
-            zipFile.addFiles(new ArrayList<File>(fileList.toList()), params);
-        } catch (Exception e) {
+            for (final File file : fileList) {
+                params.setFileNameInZip(file.getName());
+                zipFile.addFile(file, params);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
             return 1;
         }
-
         return 0;
     }
 
